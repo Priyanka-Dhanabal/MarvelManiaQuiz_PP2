934bdba8422367ef86253346a08b2543a9f3afc6
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-03-07 12:08:19 +0000

    declared questions and answer of the quiz

---
+++ assets/scripts/script.js
@@ -0,0 +1,83 @@
+const questions = [
+    {
+       question: "Who is known as the 'God of Thunder' in the Marvel Universe?",
+       answers:[
+           {text: "Iron Man", correct:false},
+           {text: "Captain America", correct:false},
+           {text: "Thor", correct:true},
+           {text: "Hulk", correct:false},
+       ]
+   },{
+       question: "What is the name of Tony Stark's AI in the Iron Man suit?",
+       answers:[
+           {text: "F.R.I.D.A.Y.", correct:false},
+           {text: "J.A.R.V.I.S.", correct:true},
+           {text: "H.E.R.B.I.E.", correct:false},
+           {text: "S.H.I.E.L.D.", correct:false},
+       ]
+   },{
+       question: "Which of these characters is not a member of the Avengers?",
+       answers:[
+           {text: "Wolverine", correct:true},
+           {text: "Black Panther", correct:false},
+           {text: "Scarlet Witch", correct:false},
+           {text: "Hawkeye", correct:false},
+       ]
+   },{
+       question: "Which Infinity Stone was embedded in Vision's forehead?",
+       answers:[
+           {text: "Power Stone", correct:false},
+           {text: "Time Stone", correct:false},
+           {text: "Reality Stone", correct:false},
+           {text: "Mind Stone", correct:true},
+       ]
+   },{
+       question: "What is the name of the fictional metal that Captain America's shield is made of?",
+       answers:[
+           {text: "Adamantium", correct:false},
+           {text: "Vibranium", correct:true},
+           {text: "Titanium", correct:false},
+           {text: "Plasteel", correct:false},
+       ]
+   },{
+       question: "What is the name of the dimension where Doctor Strange draws his mystical powers from?",
+       answers:[
+           {text: "Dark Dimension", correct:true},
+           {text: "Astral Plane", correct:false},
+           {text: "Quantum Realm", correct:false},
+           {text: "Mirror Dimension", correct:false},
+       ]
+   },{
+       question: "What is the real name of the superhero Black Widow?",
+       answers:[
+           {text: "Carol Danvers", correct:false},
+           {text: "Wanda Maximoff", correct:false},
+           {text: "Jessica Jones", correct:false},
+           {text: "Natasha Romanoff", correct:true},
+       ]
+   },{
+       question: "What is the name of the fictional planet that Thor comes from?",
+       answers:[
+           {text: "Titan", correct:false},
+           {text: "Xandar", correct:false},
+           {text: "Sakaar", correct:false},
+           {text: "Asgard", correct:true},
+       ]
+   },{
+       question: "What is the name of the powerful artifact sought after by Thanos in the Marvel Cinematic Universe?",
+       answers:[
+           {text: "Infinity Gauntlet", correct:true},
+           {text: "Aether", correct:false},
+           {text: "Orb", correct:false},
+           {text: "Tesseract", correct:false},
+       ]
+   },{
+       question: "What is the name of Scott Lang's daughter?",
+       answers:[
+           {text: "Hope", correct:false},
+           {text: "Janet", correct:false},
+           {text: "Cassie", correct:true},
+           {text: "Peggy", correct:false},
+       ]
+   }
+]
